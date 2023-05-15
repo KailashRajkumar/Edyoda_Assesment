6a633ce165d0821e190ed12b82a8beecabbc1de6
Kailash Rajkumar <93399794+KailashRajkumar@users.noreply.github.com>
2023-05-15 16:49:06 +0400

    Add files via upload

---
+++ edyoda_assignment/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+import NavBar from './Components/NavBar';
+import Text from './Components/Text';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Form />
+      <Text />
+    </>
+  );
+}
+
+export default App;
